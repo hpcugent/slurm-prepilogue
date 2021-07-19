ca03a60b538cc053784279a9de981913dfc59960
Alex Domingo <alex.domingo.toro@vub.be>
2021-07-19 16:19:08 +0200

    set PMIx direct connection in prolog

--- files/prolog.sh
+++ files/prolog.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # #
-# Copyright 2018-2018 Ghent University
+# Copyright 2018-2021 Ghent University
 #
 # This file is part of slurm-prepilogue
 # originally created by the HPC team of Ghent University (http://ugent.be/hpc/en),
@@ -22,3 +22,21 @@ for check in checkpaths.sh mps_prolog.sh; do
         exit $ec
     fi
 done
+
+# Configure PMIx direct connection mode
+# SLURM_PMIX_DIRECT_CONN: enables direct connection with TCP
+# - normally enabled by default (otherwise it's very bad because communication falls back to out-of-band mode)
+# - should work with most network setups as it only needs TCP
+# SLURM_PMIX_DIRECT_CONN_UCX: enables direct connection with UCX
+# - enabled by default if Slurm is built with PMIx and UCX
+# - IB only works with the mlx5 driver with rdma-core (supported since MOFED 4.7+ or MOFED 5.1+)
+# SLURM_PMIX_DIRECT_CONN_EARLY: enables UCX earlier in the communication setup
+# - disabled by default
+
+# Disable SLURM_PMIX_DIRECT_CONN_UCX on mlx4 as it is not supported
+# This is only needed for our ivybridge nodes with ConnectX-3, can be removed whenever those are thrown away
+[ -d "/sys/module/mlx4_core" ] && PMIX_UCX="0" || PMIX_UCX="1"
+
+export SLURM_PMIX_DIRECT_CONN="1"
+export SLURM_PMIX_DIRECT_CONN_UCX="$PMIX_UCX"
+export SLURM_PMIX_DIRECT_CONN_EARLY="$PMIX_UCX"
